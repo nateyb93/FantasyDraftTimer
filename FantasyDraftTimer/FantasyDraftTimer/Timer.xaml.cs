024f0ab9d0596c1be4160806c80066bc64c2b789
nateyb93 <brownn15@up.edu>
2015-07-28 09:18:50 -0700

    Added fonts to screen

--- FantasyDraftTimer/FantasyDraftTimer/Timer.xaml.cs
+++ FantasyDraftTimer/FantasyDraftTimer/Timer.xaml.cs
@@ -19,15 +19,30 @@ namespace FantasyDraftTimer
 {
     public sealed partial class Timer : UserControl
     {
+        /// <summary>
+        /// Time left on the countdown
+        /// </summary>
         private int _timeLeft;
 
+        /// <summary>
+        /// Total time allowed on the timer
+        /// </summary>
         public int MaxTime { get; set; }
 
+        /// <summary>
+        /// Timer object that keeps track of how much time is left
+        /// </summary>
         private DispatcherTimer _timer;
 
+        /// <summary>
+        /// MainPage control to manipulate
+        /// </summary>
         public MainPage MainPage { get; set; }
 
         private Size _size;
+        /// <summary>
+        /// Size of the appaarance of the UI
+        /// </summary>
         public Size Size
         {
             get
@@ -41,13 +56,20 @@ namespace FantasyDraftTimer
             }
         }
 
+        /// <summary>
+        /// Creates atimer object
+        /// </summary>
         public Timer()
         {
             this.InitializeComponent();
-
             Loaded += Timer_Loaded;
         }
 
+        /// <summary>
+        /// Executes when the control loads
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void Timer_Loaded(object sender, RoutedEventArgs e)
         {
             _timeLeft = MaxTime;
@@ -57,6 +79,11 @@ namespace FantasyDraftTimer
             _timer.Tick += _timer_Tick;
         }
 
+        /// <summary>
+        /// Timer tick event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void _timer_Tick(object sender, object e)
         {
             _timeLeft--;
@@ -69,6 +96,9 @@ namespace FantasyDraftTimer
             _updateText();
         }
 
+        /// <summary>
+        /// Stops the timer
+        /// </summary>
         public void StopTimer()
         {
             _timer.Stop();
@@ -76,11 +106,17 @@ namespace FantasyDraftTimer
             //PLAY SOUND
         }
 
+        /// <summary>
+        /// Starts the timer
+        /// </summary>
         public void StartTimer()
         {
             _timer.Start();
         }
 
+        /// <summary>
+        /// Updates the text of the timer
+        /// </summary>
         private void _updateText()
         {
             int seconds = _timeLeft % 60;
